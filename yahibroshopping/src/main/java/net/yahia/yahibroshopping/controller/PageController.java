e685d8ab1e58466efea20871a9e81eeb881d7670
yaabdujebar <40149486+yaabdujebar@users.noreply.github.com>
2018-08-14 11:40:42 -0500

    succesfully loaded static resources

---
+++ yahibroshopping/src/main/java/net/yahia/yahibroshopping/controller/PageController.java
@@ -0,0 +1,36 @@
+package net.yahia.yahibroshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+         mv.addObject("greeting","wellcome to spring");
+		return mv;
+	}
+//	@RequestMapping(value ="/test")
+//	public ModelAndView test(@RequestParam(value="greeting", required = false)String greeting) {
+//		if(greeting == null) {
+//			greeting = "Hello there";
+//		}
+//		ModelAndView mv = new ModelAndView("page");
+//         mv.addObject("greeting",greeting);
+//		return mv;
+//	}
+	
+//	@RequestMapping(value ="/test/{greeting}")
+//	public ModelAndView test(@PathVariable("greeting")String greeting) {
+//		if(greeting == null) {
+//			greeting = "Hello there";
+//		}
+//		ModelAndView mv = new ModelAndView("page");
+//         mv.addObject("greeting",greeting);
+//		return mv;
+//	}
+}
